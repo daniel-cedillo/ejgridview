68c446848a7395b7199988cd8a37df63119be06f
FlutLab.io <>
2022-05-02 18:12:36 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cedillo/gridview.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,45 +17,3 @@ class MyApp extends StatelessWidget {
         home: const MyHomePage());
   }
 } //widget sin estado
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-} //widget con estado
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> images = [
-    "assets/images/descarga.jpg",
-    "assets/images/descarg.jpg",
-    "assets/images/descar.jpg",
-    "assets/images/desca.jpg",
-    "assets/images/desc.jpg",
-    "assets/images/des.jpg",
-    "assets/images/image.jpg",
-    "assets/images/images.jpg",
-  ]; //lista de imagenes
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Hotel Cedillo"),
-        ),
-        body: GridView.custom(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 8,
-          ),
-          padding: const EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
